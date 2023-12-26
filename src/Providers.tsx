5d715d3c9c6f4c703a0ea6caa5124a20eb5681a0
Odin Jacques <121515166+OdinAJB@users.noreply.github.com>
2023-12-26 12:27:52 -0600

    Initial Features (#1)
    Added a card and list components and several config and functionalities

---
+++ src/Providers.tsx
@@ -0,0 +1,19 @@
+import { QueryClient, QueryClientProvider, queryOptions } from "@tanstack/react-query";
+import React from "react";
+import { BrowserRouter} from "react-router-dom";
+
+
+interface ProviderProps{
+    children: React.ReactNode;
+}
+
+
+const queryClient= new QueryClient();
+
+const Providers: React.FC<ProviderProps> = ({children}) =>(
+    <QueryClientProvider client={queryClient}>
+        <BrowserRouter>{children}</BrowserRouter>
+    </QueryClientProvider>
+)
+
+export default Providers;
