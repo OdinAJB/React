0e02fe01d2d20dc4e2dcba64a92a808c8dda2777
Odin Jacques <odin.bocanegra02@gmail.com>
2023-12-26 12:23:50 -0600

    Initial Features
    Added a card and list components and several config and functionalities

---
+++ src/components/PokemonList/PokemonList.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useGetPokemonList } from "../../hooks/useGetPokemonList";
+import { PokemonCard } from "../PokemonCard/PokenmonCard";
+import { Grid } from "../shared/Grid/Grid";
+
+const PokemonList: React.FC = () => {
+  const { pokemonList, goToNextPage, goToPreviouspage } = useGetPokemonList();
+
+  return (
+    <Grid goToNextPage={goToNextPage} goToPreviousPage={goToPreviouspage}>
+      {pokemonList?.map((pokemon) => (
+        <PokemonCard key={pokemon?.name} pokemon={pokemon} />
+      ))}
+    </Grid>
+  );
+};
+
+export default PokemonList;
