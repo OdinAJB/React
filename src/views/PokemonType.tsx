7cd39c8d70e197505b1f99ca788ad1f424667725
Odin Jacques <odin.bocanegra02@gmail.com>
2023-12-30 22:56:56 -0600

    Pokemon By Type
    Added funcionality to filter pokemons by type

---
+++ src/views/PokemonType.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { PokemonTypeList } from "../components/PokemonListByType/PokemonListByType";
+
+const PokemonByType: React.FC= () => <PokemonTypeList/>
+
+export default PokemonByType;
